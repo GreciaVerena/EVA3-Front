4436782a2f5379aded12f01a46864826f70cda4c
Eliezer Gonzalez <eliezergonzalez.maldonado@gmail.com>
2025-05-29 00:50:18 -0400

    feat: Agregar componente FAQ con preguntas frecuentes y su funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import ContactForm from './components/ContactForm/Contac.jsx'
+import FAQ from './components/FAQ/Faq.jsx';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
       <Hero />
+      <FAQ/>
       <ContactForm/>
       <Footer />
     </>
